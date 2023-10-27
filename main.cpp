65c0e0c7aea2db33140be25dc73f6f42b8099b72
ahmed-8areeb <moud123321@gmail.com>
2023-10-27 15:05:04 +0200

    first branch commit

--- main.cpp
+++ main.cpp
@@ -12,9 +12,20 @@ int vectorSum(vector<int> v)
   return sum;
 }
 
+int vectorAverage(vector<int> v)
+{
+  int sum = 0;
+  for (int i = 0; i < v.size(); i++)
+  {
+    sum += v[i];
+  }
+  return sum / v.size();
+}
+
 int main()
 {
   vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   cout << vectorSum(v) << endl;
+  cout << vectorAverage(v) << endl;
   return 0;
 }
