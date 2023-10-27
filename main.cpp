808fdcefd92424734a3683f0cac2e6607ed1eebb
ahmed-8areeb <moud123321@gmail.com>
2023-10-27 14:00:02 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int vectorSum(vector<int> v)
+{
+  int sum = 0;
+  for (int i = 0; i < v.size(); i++)
+  {
+    sum += v[i];
+  }
+  return sum;
+}
+
+int main()
+{
+  vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  cout << vectorSum(v) << endl;
+  return 0;
+}
